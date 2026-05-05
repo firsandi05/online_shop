bc865e83856822ceca1b491ec66aab9e81c58959
irsyad05btj <139347278+irsyad05btj@users.noreply.github.com>
2026-05-06 05:37:08 +0700

    Add real number

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@
 export const STORE_CONFIG = {
   // Mengambil nomor WhatsApp dari variabel lingkungan jika tersedia, 
   // atau menggunakan nomor template default
-  whatsappNumber: (typeof window !== 'undefined' && window.CONFIG_WHATSAPP_NUMBER) || 
-                  "628123456789" 
+  whatsappNumber: (typeof window !== 'undefined' && window.CONFIG_WHATSAPP_NUMBER) ||
+    "6282240354425"
 };
