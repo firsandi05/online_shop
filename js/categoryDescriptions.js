55fcbc1549e581390aa99016d4673c545497a6f6
xwingproject-web <xwingproject@gmail.com>
2026-08-22 13:15:39 +0700

    Update categoryDescriptions.js

--- js/categoryDescriptions.js
+++ js/categoryDescriptions.js
@@ -17,7 +17,85 @@ export const CATEGORY_DESCRIPTIONS = {
     1: {
 
         id:
-            'Tulis deskripsi kategori pertama di sini.',
+            'JAM TANGAN  PRIA / WANITA / ANAK / PEREMPUAN / LAKI LAKI / REMAJA | JAM TANGAN UNIK LUCU
+
+
+
+Ayo dapatkan Jam Tangan Custom brick Exclusive dari Xwing_project !
+
+
+
+Details :
+
+
+
+✔️ Bricks ORIGINAL 100%
+
+✔️ Diameter Jam 3.4 cm 
+
+✔️ Kepala jam nya NON ORIGINAL yaa 
+
+✔️ bonus baterai 1 pc
+
+✔️ Dapat strap jam bawaan 
+
+✔️  Bonus Obeng plus  (+)   
+
+✔️ Kartu ucapan dari Xwing Project 
+
+
+
+❗️ SIZE:
+
+
+
+ALL SIZE / ADJUSTABLE (bisa dibesar - kecilkan) yaa..
+
+
+
+❗️Standard ukuran SMALL adalah untuk lingkar pergelangan tangan maksimal 12 cm.
+
+❗️Standard ukuran MEDIUM adalah untuk lingkar pergelangan tangan maksimal 15 cm.
+
+❗️Standard ukuran LARGE adalah untuk lingkar pergelangan tangan 15 - 20 cm.
+
+❗️ Standard ukuran XTRA LARGE adalah untuk lingkar pergelangan tangan 20 - 25 cm.
+
+❗️ Harap cantumkan ukuran lingkar tangan yaa bro sistt.
+
+
+
+Namun mohon maaf, tanpa bermaksud menyinggung, untuk yang kurus sekali atau malah body lebih berisi, mohon beritahu kami terlebih dahulu agar jumlah brick & harganya bisa kami sesuaikan.
+
+
+
+❗️ NOTES:
+
+-  Produk akan dikirim MAX dalam 2-3 hari kerja, TAPI tidak menutup kemungkinan produk sis/gan akan dikirim pada H+1, namun mohon tidak memburu kami ya.
+
+
+
+- Setelah checkout kami anggap telah membaca notes ini.
+
+
+
+Kami telah dipercaya oleh Lebih dari 300 ++ customer yang telah memberikan testimoni kepuasannya.. Buktikan sendiri! :)
+
+
+
+Order SEKARANG sebelum harga naik sewaktu-waktu! :)
+
+Terimakasih
+
+
+
+UNTUK KOMPLAIN HARUS DAN WAJIB DISERTAKAN VIDEO UNBOXING/ VIDEO BUKA PAKET YAA
+
+TANPA ITU KAMI TIDAK MENERIMA KOMPLAIN, JIKALAU VIDEO UNBOXING/ BUKA PAKET MEMANG SUDAH ADA, KAMI LANGSUNG PROSES KOMPLAIN TANPA HAMBATAN.
+
+
+
+#jamtangan #jamimut #jamlucu #jammurah #jamunik #jamkekinian #diyjam #kado #kadoulangtahun#kadoultah #kadonikah #kadospesial #kadounik #kadokekinian #toystory #lego #buzzlightyear #legowatch #watch #presentgift #gift #special #present #diyclock #watch #lego #legoori #legomurah #legoaccesories #gelanglego #kalunglego #mainananak #aksesorisanak #kado #kadounik #jamlego #jamtangan #iwatch #iwatchstrap #strapiwatch #swatch .',
 
         en:
             'Write the description for the first category here.'
